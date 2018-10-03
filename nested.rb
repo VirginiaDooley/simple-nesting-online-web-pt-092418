2b15d21eecb3c5d70083acb1ad6db804d292295c
Virginia Dooley <vcdooley@gmail.com>
2018-10-03 09:32:58 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -79,7 +79,7 @@ def adding_matz
 
 yukihiro_matsumoto = [:known_for => "Ruby"][:languages => ["LISP", "C"]
 
-programmer_hash[]
+programmer_hash[:yukihiro_matsumoto]
 
 programmer_hash(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]})
 binding.pry
