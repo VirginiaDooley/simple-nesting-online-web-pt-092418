6fd4adbd6d931c4031d24a2cff6a4b7787152b57
Virginia Dooley <vcdooley@gmail.com>
2018-10-03 09:33:24 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -77,12 +77,10 @@ def adding_matz
         }
      }
 
-yukihiro_matsumoto = [:known_for => "Ruby"][:languages => ["LISP", "C"]
+programmer_hash[:yukihiro_matsumoto][:known_for => "Ruby"][:languages => ["LISP", "C"]
 
-programmer_hash[:yukihiro_matsumoto]
-
-programmer_hash(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]})
-binding.pry
+# programmer_hash(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]})
+# binding.pry
 programmer_hash
 end
 
